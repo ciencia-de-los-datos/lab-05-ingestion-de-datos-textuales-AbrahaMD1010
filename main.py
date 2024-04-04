3c92876c019e6388aba632f7c65c8cecbb6dbef3
Abraham Miguel <amiguel@unal.edu.co>
2024-04-04 14:58:05 -0500

    b

---
+++ main.py
@@ -0,0 +1,33 @@
+import pandas as pd
+import numpy as np
+from pathlib import Path
+
+
+carpeta_data = Path("./data")
+test = []
+train = []
+for carpeta in carpeta_data.iterdir():
+    if carpeta.name == "__MACOSX":
+        continue
+
+    print(carpeta)
+    for estado in carpeta.iterdir():
+        if estado.name == ".DS_Store":
+            continue
+
+        print("\t", estado)
+        for archivo in estado.glob("*.txt"):
+            frase = archivo.read_text()
+            sentimiento = estado.name
+            if carpeta.name == "test":
+                test.append((frase, sentimiento))
+
+            if carpeta.name == "train":
+                train.append((frase, sentimiento))
+
+
+test_df = pd.DataFrame(test, columns=["phrase", "sentiment"])
+train_df = pd.DataFrame(train, columns=["phrase", "sentiment"])
+
+train_df.to_csv("train_dataset.csv", index=False)
+test_df.to_csv("test_dataset.csv", index=False)
